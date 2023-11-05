d9221ed9808629dc1bbb6c285ee0dd06b767c06d
IgMiras <im.miras@unesp.br>
2023-11-05 16:33:15 -0300

    classes de pagamento adicionadas

---
+++ backend/classes/Boleto.js
@@ -0,0 +1,20 @@
+import Pagamento from './Pagamento';
+
+export default class Boleto extends Pagamento {
+	constructor(codigoNota, numero) {
+		super(codigoNota);
+		this._numero = numero;
+	}
+
+	get numero() {
+		return this._numero;
+	}
+
+	set numero(numero) {
+		this._numero = numero;
+	}
+
+	gerarCodigoBoleto() {
+		return Math.floor(Math.random() * 100);
+	}
+}
