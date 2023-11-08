3891f2c750af453593736946239bf5767822f43b
Sabrina <sapailiv@gmail.com>
2023-11-08 17:20:59 -0300

    tentando setar a fonte global

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -3,10 +3,12 @@ import { Layout } from "@/components/Layout";
 import { SubHome1 } from "@/components/HomeSec01";
 import "@/styles/globals.css";
 import { SubHome2 } from "@/components/HomeSec02";
+import GlobalFonts from '../Fonts/fonts'
 
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
+      <GlobalFonts />
       <Home />
       <SubHome1 />
       <SubHome2 />
