74c529c9dd4d0224e3c83965c0afb9ca5d30417d
LaisIsabella <lais.isabella@unesp.br>
2023-11-09 23:26:44 -0300

    FONTE GLOBAL DEU CERTO FINALMENTE!!!!

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -3,15 +3,55 @@ import { Layout } from "@/components/Layout";
 import { SubHome1 } from "@/components/HomeSec01";
 import "@/styles/globals.css";
 import { SubHome2 } from "@/components/HomeSec02";
-import GlobalFonts from '../Fonts/fonts'
+import { ThemeProvider } from "styled-components";
+import { customTheme } from "@/themes";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <GlobalFonts />
+    <ThemeProvider theme={customTheme}>
+      <Head>
+        <title>Grupo GV</title>
+        <meta
+          name="description"
+          content="O GRUPO GV Consultoria Farmacêutica é uma empresa composta por duas áreas do segmento farmacêutico, uma empresa sendo de Consultoria e Assessoria Farmacêutica Magistral, e a outra, de Projetos e Equipamentos para abertura de Laboratório, Farmácia de Manipulação e Drogarias."
+        />
+        <meta
+          name="keywords"
+          content="consultoria farmacêutica, grupo GV, farmácia, equipamentos laboratoriais, manipulação, farmacêutico, drogaria, laboratório"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        {/* <link rel="canonical" href="https://www.grupogv.com/" /> */}
+        <meta
+          property="og:title"
+          content="Grupo GV - Consultoria Farmacêutica"
+        />
+        <meta
+          property="og:description"
+          content="O GRUPO GV Consultoria Farmacêutica é uma empresa composta por duas áreas do segmento farmacêutico, uma empresa sendo de Consultoria e Assessoria Farmacêutica Magistral, e a outra, de Projetos e Equipamentos para abertura de Laboratório, Farmácia de Manipulação e Drogarias."
+        />
+        <meta name="robots" content="index, follow" />
+        <meta property="og:image" content="/favicon.png" />
+        {/* <meta property="og:url" content="https://www.grupogv.com/" /> */}
+        <link rel="icon" href="/favicon.ico" />
+
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Kanit&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      {/* <HomeLayout> */}
+      {/* <HomeQuemSomos />
+        <HomeValores /> */}
+      {/* <ConsultoriaCursos /> */}
       <Home />
-      <SubHome1 />
-      <SubHome2 />
-    </Layout>
+      {/* <CardProdutos /> */}
+      {/* <ButtonCategoria texto="Lais" isSelected={isSelected} setIsSelected={setIsSelect} /> */}
+      {/* <HeaderPesquisa /> */}
+      {/* <ConsultoriaPalestras /> */}
+      {/* </HomeLayout> */}
+    </ThemeProvider>
   );
 }
--- frontend/src/themes/index.jsx
+++ frontend/src/themes/index.jsx
@@ -1,1 +1,31 @@
+export const customTheme = {
+  colors: {
+    primary: "#ABD930",
+    secondary: "#5FC2CE",
+    softBlue: "#3E8AA1",
+    shadowBlue: "#20404A",
+    darkBlue: "#2B5E6B",
+    darkGreen: "#849913",
+    black: "#0B0B0B",
+    white: "#ECECEC",
+    gray: "#282828",
+  },
 
+  fontSize: {
+    title: "1.5rem",
+    subtitle: "1.2rem",
+    text: "1rem",
+  },
+
+  fontWeight: {
+    title: "bold",
+    subtitle: "bold",
+    text: "regular",
+  },
+
+  fontHeightLine: {
+    title: "2rem",
+    subtitle: "1.8rem",
+    text: "1.5rem",
+  },
+};
