06245e908ebff4c7b52f911dfbce8266f8508613
IgMiras <im.miras@unesp.br>
2023-11-13 20:24:28 -0300

    Cadastro de jogos criado, alem de mudancas em classes

--- backend/controllers/JogoController.js
+++ backend/controllers/JogoController.js
@@ -4,47 +4,68 @@ import Aventura from '../classes/Aventura.js';
 import Corrida from '../classes/Corrida.js';
 import Esporte from '../classes/Esporte.js';
 import RPG from '../classes/RPG.js';
+import DesenvolvedoraModel from '../models/DesenvolvedoraModel.js';
+import JogoModel from '../models/JogoModel.js';
 import FactoryJogo from '../factory/FactoryJogo.js';
 
-async function cadastrarJogo(req, res) {
-	const {
-		codigo,
-		nome,
-		descricao,
-		desenvolvedora,
-		dataLancamento,
-		valor,
-		requisitosMinimos,
-		disponivel,
-		ehFisico,
-		tipoJogo,
-	} = req.body;
+export default async function cadastrarJogo(req, res) {
+    const {
+        nome,
+        descricao,
+        nomeDesenvolvedora,
+        dataLancamento,
+        valor,
+        requisitosMinimos,
+        disponivel,
+        ehFisico,
+        tipoJogo,
+    } = req.body;
 
-	const jogo = FactoryJogo.factoryMethod(
-		codigo,
-		nome,
-		descricao,
-		desenvolvedora,
-		dataLancamento,
-		valor,
-		requisitosMinimos,
-		disponivel,
-		ehFisico,
-		tipoJogo
-	);
+    const jogo = FactoryJogo.factoryMethod(
+        nome,
+        descricao,
+        nomeDesenvolvedora,
+        dataLancamento,
+        valor,
+        requisitosMinimos,
+        disponivel,
+        ehFisico,
+        tipoJogo
+    );
+
+    try {
+        const desenvolvedora = await DesenvolvedoraModel.findOne(
+            nomeDesenvolvedora
+        );
+
+        if (!desenvolvedora) {
+            return res
+                .status(404)
+                .json({ msg: 'Desenvolvedora nao existente' });
+        }
+
+        const jogosFields = {};
+        if (nome) jogosFields.nome = nome;
+        if (descricao) jogosFields.descricao = descricao;
+        jogosFields.desenvolvedora = desenvolvedora.id; // Obrigatorio
+        if (dataLancamento) jogosFields.dataLancamento = dataLancamento;
+        if (valor) jogosFields.valor = jogo.valor; // Imposto calculado no construtor
+        if (requisitosMinimos)
+            jogosFields.requisitosMinimos = requisitosMinimos;
+        if (typeof disponivel !== 'undefined') {
+            jogosFields.disponivel = disponivel;
+        }
+        if (typeof ehFisico !== 'undefined') {
+            jogosFields.ehFisico = ehFisico;
+        }
+        if (tipoJogo) jogosFields.tipoJogo = tipoJogo;
+
+        let jogoModel = new JogoModel(jogosFields);
+
+        await jogoModel.save();
+        res.json(jogoModel);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Erro de Servidor');
+    }
 }
-
-const acao = new Acao(
-	123,
-	'aa',
-	'asdasd',
-	'sega',
-	'2021-01-01',
-	100,
-	'asdasd',
-	true,
-	true,
-	'acao'
-);
-
-console.log(acao);
