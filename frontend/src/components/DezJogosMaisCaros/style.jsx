9425e8da05abc9485f3834a7abe80ef97743366a
LaisIsabella <lais.isabella@unesp.br>
2023-11-18 15:48:47 -0300

    Tela de listagem de jogos mais caros

---
+++ frontend/src/components/DezJogosMaisCaros/style.jsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  padding: 95px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  a {
+    font-weight: bold;
+    font-size: 2.8rem;
+    color: #013757;
+  }
+`;
+
+export const Content = styled.div`
+  border: 0.2px solid #01769a;
+  border-radius: 3%;
+  width: 70%;
+  height: auto;
+  display: flex;
+  flex-direction: column;
+  margin-top: 5%;
+`;
--- frontend/src/components/Equipamentos/CardListagem/style.jsx
+++ frontend/src/components/Equipamentos/CardListagem/style.jsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   margin: 5%;
-  width: 50%;
+  width: 90%;
   height: 150px;
   border-radius: 10px;
 
