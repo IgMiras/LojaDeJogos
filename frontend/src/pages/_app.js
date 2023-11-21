f009626c2c290d159640b491bcab2011e29244c3
Sabrina <sapailiv@gmail.com>
2023-11-21 13:50:33 -0300

    footer finalizado

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -1,15 +1,11 @@
-import { Home } from "@/components/Home";
-import { Layout } from "@/components/Layout";
-import { SubHome1 } from "@/components/HomeSec01";
+
 import "@/styles/globals.css";
-import { SubHome2 } from "@/components/HomeSec02";
+import{ Footer } from "@/components/Footer/index";
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Home />
-      <SubHome1 />
-      <SubHome2 />
-    </Layout>
+    
+      <Footer/>
+   
   );
 }
