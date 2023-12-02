a0ade2d675dc5602340daf99e15738768f6d3116
IgMiras <im.miras@unesp.br>
2023-12-02 14:58:28 -0300

    strategy pattern funcionando

--- backend/controllers/JogoController.js
+++ backend/controllers/JogoController.js
@@ -8,224 +8,229 @@ const DesenvolvedoraModel = require('../models/DesenvolvedoraModel.js');
 const JogoModel = require('../models/JogoModel.js');
 const FactoryJogo = require('../factory/FactoryJogo.js');
 const {
-    OrdenacaoStrategy,
-    QuickSort,
-    BubbleSort,
+	OrdenacaoStrategy,
+	QuickSort,
+	BubbleSort,
 } = require('../strategy/OrdenacaoStrategy.js');
 
 async function cadastrarJogo(req, res) {
-    const {
-        nome,
-        descricao,
-        nomeDesenvolvedora,
-        dataLancamento,
-        valor,
-        requisitosMinimos,
-        tipoJogo,
-        disponivel,
-        ehFisico,
+	const {
+		nome,
+		descricao,
+		nomeDesenvolvedora,
+		dataLancamento,
+		valor,
+		requisitosMinimos,
+		tipoJogo,
+		disponivel,
+		ehFisico,
 
-        linkImagem,
-    } = req.body;
+		linkImagem,
+	} = req.body;
 
-    const jogo = FactoryJogo.factoryMethod(
-        nome,
-        descricao,
-        nomeDesenvolvedora,
-        dataLancamento,
-        valor,
-        requisitosMinimos,
-        disponivel,
-        ehFisico,
-        tipoJogo
-    );
+	const jogo = FactoryJogo.factoryMethod(
+		nome,
+		descricao,
+		nomeDesenvolvedora,
+		dataLancamento,
+		valor,
+		requisitosMinimos,
+		disponivel,
+		ehFisico,
+		tipoJogo
+	);
 
-    try {
-        // Ver se a desenvolvedora existe
-        const desenvolvedora = await DesenvolvedoraModel.findOne({
-            nome: nomeDesenvolvedora,
-        });
+	try {
+		// Ver se a desenvolvedora existe
+		const desenvolvedora = await DesenvolvedoraModel.findOne({
+			nome: nomeDesenvolvedora,
+		});
 
-        if (!desenvolvedora) {
-            return res
-                .status(404)
-                .json({ msg: 'Desenvolvedora nao existente' });
-        }
+		if (!desenvolvedora) {
+			return res.status(404).json({ msg: 'Desenvolvedora nao existente' });
+		}
 
-        // Preencher um objeto com os campos do jogo
-        const jogosFields = {};
-        if (nome) jogosFields.nome = nome;
-        if (descricao) jogosFields.descricao = descricao;
-        jogosFields.desenvolvedora = desenvolvedora.id; // Obrigatorio
-        if (dataLancamento) jogosFields.dataLancamento = dataLancamento;
-        if (valor) jogosFields.valor = jogo.valor; // Imposto calculado no construtor
-        if (requisitosMinimos)
-            jogosFields.requisitosMinimos = requisitosMinimos;
-        if (typeof disponivel !== 'undefined') {
-            jogosFields.disponivel = disponivel;
-        }
-        if (typeof ehFisico !== 'undefined') {
-            jogosFields.ehFisico = ehFisico;
-        }
-        if (tipoJogo) jogosFields.tipoJogo = tipoJogo;
-        if (linkImagem) jogosFields.linkImagem = linkImagem;
+		// Preencher um objeto com os campos do jogo
+		const jogosFields = {};
+		if (nome) jogosFields.nome = nome;
+		if (descricao) jogosFields.descricao = descricao;
+		jogosFields.desenvolvedora = desenvolvedora.id; // Obrigatorio
+		if (dataLancamento) jogosFields.dataLancamento = dataLancamento;
+		if (valor) jogosFields.valor = jogo.valor; // Imposto calculado no construtor
+		if (requisitosMinimos) jogosFields.requisitosMinimos = requisitosMinimos;
+		if (typeof disponivel !== 'undefined') {
+			jogosFields.disponivel = disponivel;
+		}
+		if (typeof ehFisico !== 'undefined') {
+			jogosFields.ehFisico = ehFisico;
+		}
+		if (tipoJogo) jogosFields.tipoJogo = tipoJogo;
+		if (linkImagem) jogosFields.linkImagem = linkImagem;
 
-        // Ver se o jogo ja existe
-        let jogoModel = await JogoModel.findOne({ nome });
+		// Ver se o jogo ja existe
+		let jogoModel = await JogoModel.findOne({ nome });
 
-        if (jogoModel) {
-            return res.status(400).json({ msg: 'Jogo ja existente' });
-        }
+		if (jogoModel) {
+			return res.status(400).json({ msg: 'Jogo ja existente' });
+		}
 
-        // Criar novo jogo
-        jogoModel = new JogoModel(jogosFields);
+		// Criar novo jogo
+		jogoModel = new JogoModel(jogosFields);
 
-        await jogoModel.save();
-        res.status(200).json(jogoModel);
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Erro de Servidor');
-    }
+		await jogoModel.save();
+		res.status(200).json(jogoModel);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Erro de Servidor');
+	}
 }
 
 async function listarTodosJogos(req, res) {
-    try {
-        const jogos = await JogoModel.find().populate('desenvolvedora');
-        res.json(jogos);
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Erro de Servidor');
-    }
+	try {
+		const jogos = await JogoModel.find().populate('desenvolvedora');
+		res.json(jogos);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Erro de Servidor');
+	}
 }
 
 async function listarJogosAcao(req, res) {
-    try {
-        const jogos = await JogoModel.find({ tipoJogo: 'Acao' }).populate(
-            'desenvolvedora'
-        );
-        res.json(jogos);
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Erro de Servidor');
-    }
+	try {
+		const jogos = await JogoModel.find({ tipoJogo: 'Acao' }).populate(
+			'desenvolvedora'
+		);
+		res.json(jogos);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Erro de Servidor');
+	}
 }
 
 async function listarJogosAventura(req, res) {
-    try {
-        const jogos = await JogoModel.find({ tipoJogo: 'Aventura' }).populate(
-            'desenvolvedora'
-        );
-        res.json(jogos);
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Erro de Servidor');
-    }
+	try {
+		const jogos = await JogoModel.find({ tipoJogo: 'Aventura' }).populate(
+			'desenvolvedora'
+		);
+		res.json(jogos);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Erro de Servidor');
+	}
 }
 
 async function listarJogosRPG(req, res) {
-    try {
-        const jogos = await JogoModel.find({ tipoJogo: 'RPG' }).populate(
-            'desenvolvedora'
-        );
-        res.json(jogos);
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Erro de Servidor');
-    }
+	try {
+		const jogos = await JogoModel.find({ tipoJogo: 'RPG' }).populate(
+			'desenvolvedora'
+		);
+		res.json(jogos);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Erro de Servidor');
+	}
 }
 
 async function listarJogosEsporte(req, res) {
-    try {
-        const jogos = await JogoModel.find({ tipoJogo: 'Esporte' }).populate(
-            'desenvolvedora'
-        );
-        res.json(jogos);
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Erro de Servidor');
-    }
+	try {
+		const jogos = await JogoModel.find({ tipoJogo: 'Esporte' }).populate(
+			'desenvolvedora'
+		);
+		res.json(jogos);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Erro de Servidor');
+	}
 }
 
 async function listarJogosCorrida(req, res) {
-    try {
-        const jogos = await JogoModel.find({ tipoJogo: 'Corrida' }).populate(
-            'desenvolvedora'
-        );
-        res.json(jogos);
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Erro de Servidor');
-    }
+	try {
+		const jogos = await JogoModel.find({ tipoJogo: 'Corrida' }).populate(
+			'desenvolvedora'
+		);
+		res.json(jogos);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Erro de Servidor');
+	}
 }
 
 async function listarDezJogosMaisCaros(req, res) {
-    try {
-        const jogos = await JogoModel.find()
-            .populate('desenvolvedora')
-            .sort({ valor: -1 })
-            .limit(10);
-        res.json(jogos);
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Erro de Servidor');
-    }
+	try {
+		const jogos = await JogoModel.find()
+			.populate('desenvolvedora')
+			.sort({ valor: -1 })
+			.limit(10);
+		res.json(jogos);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Erro de Servidor');
+	}
 }
 
 async function listarDezJogosMaisBaratos(req, res) {
-    try {
-        const jogos = await JogoModel.find()
-            .populate('desenvolvedora')
-            .sort({ valor: 1 })
-            .limit(10);
-        res.json(jogos);
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Erro de Servidor');
-    }
+	try {
+		const jogos = await JogoModel.find()
+			.populate('desenvolvedora')
+			.sort({ valor: 1 })
+			.limit(10);
+		res.json(jogos);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Erro de Servidor');
+	}
 }
 
 async function listarTodosJogosQuickSort(req, res) {
-    try {
-        const jogos = await JogoModel.find().lean().exec();
-        const jogosArray = Array.from(jogos);
-        const ordenacaoStrategy = new OrdenacaoStrategy();
-        const quickSort = new QuickSort();
-        ordenacaoStrategy.strategy = quickSort;
-        ordenacaoStrategy.ordenar(jogosArray);
+	try {
+		const jogos = await JogoModel.find()
+			.populate('desenvolvedora')
+			.lean()
+			.exec();
+		const jogosArray = Array.from(jogos);
+		const ordenacaoStrategy = new OrdenacaoStrategy();
+		const quickSort = new QuickSort();
+		ordenacaoStrategy.strategy = quickSort;
+		console.log(ordenacaoStrategy);
+		console.log(quickSort);
+		ordenacaoStrategy.ordenar(jogosArray);
 
-        res.json(jogosArray);
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Erro de Servidor');
-    }
+		res.json(jogosArray);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Erro de Servidor');
+	}
 }
 
 async function listarTodosJogosBubbleSort(req, res) {
-    try {
-        const jogos = await JogoModel.find().lean().exec();
-        const jogosArray = Array.from(jogos);
-        const ordenacaoStrategy = new OrdenacaoStrategy();
-        const bubbleSort = new BubbleSort();
-        ordenacaoStrategy.strategy = bubbleSort;
-        ordenacaoStrategy.ordenar(jogosArray);
+	try {
+		const jogos = await JogoModel.find()
+			.populate('desenvolvedora')
+			.lean()
+			.exec();
+		const jogosArray = Array.from(jogos);
+		const ordenacaoStrategy = new OrdenacaoStrategy();
+		const bubbleSort = new BubbleSort();
+		ordenacaoStrategy.strategy = bubbleSort;
+		ordenacaoStrategy.ordenar(jogosArray);
 
-        res.json(jogosArray);
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Erro de Servidor');
-    }
+		res.json(jogosArray);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Erro de Servidor');
+	}
 }
 
 module.exports = {
-    cadastrarJogo,
-    listarTodosJogos,
-    listarJogosAcao,
-    listarJogosAventura,
-    listarJogosRPG,
-    listarJogosEsporte,
-    listarJogosCorrida,
-    listarDezJogosMaisCaros,
-    listarDezJogosMaisBaratos,
-    listarTodosJogosQuickSort,
-    listarTodosJogosBubbleSort,
+	cadastrarJogo,
+	listarTodosJogos,
+	listarJogosAcao,
+	listarJogosAventura,
+	listarJogosRPG,
+	listarJogosEsporte,
+	listarJogosCorrida,
+	listarDezJogosMaisCaros,
+	listarDezJogosMaisBaratos,
+	listarTodosJogosQuickSort,
+	listarTodosJogosBubbleSort,
 };
